d278662128ef330e320237e54fe6da09229271fb
azavjo40 <pabek92@gmail.com>
2021-12-23 15:53:50 +0100

    add encrypt

--- api/bank-monolithic/src/auth/interceptor/change-body.interceptor.ts
+++ api/bank-monolithic/src/auth/interceptor/change-body.interceptor.ts
@@ -6,13 +6,20 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import jwt_decode from 'jwt-decode';
+import { EncryptService } from 'src/encrypt/encrypt.service';
 
 @Injectable()
-export class LoggingInterceptor implements NestInterceptor {
+export class ChangeBodyInterceptor implements NestInterceptor {
+  constructor(private encryptService: EncryptService) {}
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const req = context.switchToHttp().getRequest();
-    if (!req.body?.bodyCrypto) return next.handle();
-    req.body = { lastName: 'Sufiev' };
-    return next.handle().pipe(map((body) => (body = 'hello from intercept')));
+    const jwtToken = context.switchToHttp().getRequest().headers.authorization;
+    // const decoded: any = jwt_decode(jwtToken);
+    console.log(jwtToken);
+    if (!req.body) return next.handle();
+    // req.body = { lastName: 'Sufiev' };
+    // return next.handle().pipe(map((body) => body));
+    return next.handle();
   }
 }
