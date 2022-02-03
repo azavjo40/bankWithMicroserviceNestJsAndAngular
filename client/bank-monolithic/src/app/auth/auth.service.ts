c0cb4b9d093544bc68e19e71f2cdd828a2adb9c2
azavjo40 <pabek92@gmail.com>
2022-02-03 19:30:09 +0100

    add check is auth

--- client/bank-monolithic/src/app/auth/auth.service.ts
+++ client/bank-monolithic/src/app/auth/auth.service.ts
@@ -10,13 +10,11 @@ export class AuthService {
   constructor(private appService: AppService) {}
   subject = new Subject();
   getIsAuth(): Observable<any> {
+    return this.subject;
+  }
+  checkIsAuth(): void {
     const storage = this.appService.getItemLocal(saveDataUser);
-    if (storage?.access_token) {
-      this.subject.next(true);
-      return this.subject;
-    } else {
-      this.subject.next(false);
-      return this.subject;
-    }
+    if (storage?.access_token) this.subject.next(true);
+    else this.subject.next(false);
   }
 }
